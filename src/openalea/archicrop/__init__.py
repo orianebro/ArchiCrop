a5c643a8a62cf19be8cb3d9f78fadd7ca947856a
thomasarsouze <thomas.arsouze@zoho.com>
2024-05-22 15:40:33 +0200

    Update oa3.yml and pyproject.yml. Also renaming to "openalea.archicrop"

--- src/openalea/archicrop/__init__.py
+++ src/openalea/archicrop/__init__.py
@@ -4,8 +4,4 @@ Copyright (c) 2024 orianebro. All rights reserved.
 ArchiCrop: 3D plant model that gives structure to crop model outputs .
 """
 
-from __future__ import annotations
-
-# from ._version import version as __version__
-
-# __all__ = ["__version__"]
+from ._version import version as __version__
